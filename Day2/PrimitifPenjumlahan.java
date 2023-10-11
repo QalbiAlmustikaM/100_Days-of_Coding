a02da28a2740acf25143ded32832d6ee671c78ba
QalbiAlmustikaM <qalbimustika25@gmail.com>
2023-10-11 23:25:41 +0800

    Day2

---
+++ Day2/PrimitifPenjumlahan.java
@@ -0,0 +1,14 @@
+package Day2;
+
+public class PrimitifPenjumlahan {
+    public static void main(String[] args) {
+        int angkaPertama = 5;
+        int angkaKedua = 3;
+
+        // Melakukan penjumlahan
+        int hasil = angkaPertama + angkaKedua;
+
+        // Menampilkan hasil
+        System.out.println("Hasil penjumlahan: " + hasil);
+    }
+}
