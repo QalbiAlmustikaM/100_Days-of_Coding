2614c897c3798e8b690fb7c10ff4e93be0eaa7e8
QalbiAlmustikaM <qalbimustika25@gmail.com>
2023-10-13 18:59:15 +0800

    Day5

---
+++ Day5/InputOutput.java
@@ -0,0 +1,19 @@
+package Day5;
+
+import java.util.Scanner;
+
+public class InputOutput {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        //input
+        System.out.print("Masukkan sebuah angka: ");
+        int angka = scanner.nextInt();
+
+        //Menampilkan output
+        System.out.println("Anda memasukkan angka: " + angka);
+
+        scanner.close();
+    }
+    
+}
