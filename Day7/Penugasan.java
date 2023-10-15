2b96d9177355449f443518051622956ea62fb635
QalbiAlmustikaM <qalbimustika25@gmail.com>
2023-10-15 22:40:19 +0800

    Day7

---
+++ Day7/Penugasan.java
@@ -0,0 +1,30 @@
+package Day7;
+
+public class Penugasan {
+    public static void main(String[] args) {
+        //Inisialisasi variabel
+        int a = 10;
+        int b = 20;
+
+        //Penugasan 
+        a = b;
+
+        //Cetak nilai variabel
+        System.out.println("Nilai a: " + a);
+        System.out.println("Nilai b: " + b);
+
+        //Penugasan dengan operasi aritmatika
+        a = a + b;
+
+        //Cetak nilai variabel
+        System.out.println("Nilai a: " + a);
+        System.out.println("Nilai b: " + b);
+
+        //Penugasan dengan variabel lain
+        a = b * 2;
+
+        //Cetak nilai variabel
+        System.out.println("Nilai a: " + a);
+        System.out.println("Nilai b: " + b);
+    }
+}
