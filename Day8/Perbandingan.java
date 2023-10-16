7bfc2615ed23943c5ad9b703a19b49fa556fe073
QalbiAlmustikaM <qalbimustika25@gmail.com>
2023-10-16 18:57:03 +0800

    Day8

---
+++ Day8/Perbandingan.java
@@ -0,0 +1,16 @@
+package Day8;
+
+public class Perbandingan {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        // Membandingkan dua bilangan bulat
+        System.out.println("a > b: " + (a > b));
+        System.out.println("a < b: " + (a < b));
+        System.out.println("a == b: " + (a == b));
+        System.out.println("a != b: " + (a != b));
+        System.out.println("a >= b: " + (a >= b));
+        System.out.println("a <= b: " + (a <= b));
+    }
+}
