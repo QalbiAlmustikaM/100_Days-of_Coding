9445127fa4b0873a744ca40cae2bd0a2c0912c7c
QalbiAlmustikaM <qalbimustika25@gmail.com>
2023-10-18 14:04:59 +0800

    Day10

---
+++ Day10/LogikaIf.java
@@ -0,0 +1,14 @@
+package Day10;
+
+public class LogikaIf {
+    public static void main(String[] args) {
+        // Logika IF
+        int angka = 20;
+        if (angka > 7){
+            System.out.println("Angka Lebih Besar Dari 7");
+        }else{
+            System.out.println("Angka Tidak Lebih besar dari 7");
+        }
+    }
+    
+}
