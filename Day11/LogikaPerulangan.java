1700857e1ffba1470d702087863f74bee51baecd
QalbiAlmustikaM <qalbimustika25@gmail.com>
2023-10-19 10:23:29 +0800

    Day11

---
+++ Day11/LogikaPerulangan.java
@@ -0,0 +1,10 @@
+package Day11;
+
+public class LogikaPerulangan {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Perulangan ke-" + i);
+        }
+    }
+    
+}
