bf7142bc64871ee2e2a7652ef554c583060be00c
QalbiAlmustikaM <qalbimustika25@gmail.com>
2023-10-21 10:16:13 +0800

    Day13

---
+++ Day13/PercabanganIfElseIfElse.java
@@ -0,0 +1,16 @@
+package Day13;
+
+public class PercabanganIfElseIfElse {
+     public static void main(String[] args) {
+        int bilangan = 10;
+
+        if (bilangan > 10) {
+            System.out.println("Bilangan tersebut lebih besar dari 10");
+        } else if (bilangan > 7) {
+            System.out.println("Bilangan tersebut lebih besar dari 7");
+        } else {
+            System.out.println("Bilangan tersebut tidak lebih dari 7");
+        }
+    }
+    
+}
