ffe48fb1dced96879673c00e8543dddeb233209d
QalbiAlmustikaM <qalbimustika25@gmail.com>
2023-10-24 19:51:49 +0800

    Day16

---
+++ Day16/PerulanganForUpTo.java
@@ -0,0 +1,14 @@
+package Day16;
+
+public class PerulanganForUpTo {
+    public static void main(String[] args) {
+        // Perulangan for up to untuk mencetak angka 1 hingga 6
+        int awal = 1;
+        int batas = 6;
+        
+        for (int i = awal; i <= batas; i++) {
+            System.out.println("Iterasi ke-" + i);
+        }
+    }
+    
+}
