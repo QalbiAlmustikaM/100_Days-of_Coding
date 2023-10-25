7a1999b52ae78d55cb27cbdbcef3f200e186c50d
QalbiAlmustikaM <qalbimustika25@gmail.com>
2023-10-25 17:47:53 +0800

    Day17

---
+++ Day17/VolumeKubus.java
@@ -0,0 +1,14 @@
+package Day17;
+
+public class VolumeKubus {
+    public static void main(String[] args) {
+        // Deklarasi Variabel
+        int sisi = 12;
+        
+        // Hitung Volume Kubus
+        int volume = sisi * sisi * sisi;
+        
+        // Tampilkan
+        System.out.println("Volume Kubus: " + volume);
+    }
+}
