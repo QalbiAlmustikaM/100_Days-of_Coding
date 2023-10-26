90e68d447ac4fed31916831e20659c4786820b10
QalbiAlmustikaM <qalbimustika25@gmail.com>
2023-10-26 15:21:04 +0800

    Day18

---
+++ Day18/LoopForMundur.java
@@ -0,0 +1,11 @@
+package Day18;
+
+public class LoopForMundur {
+    public static void main(String[] args) {
+        for (int i = 8; i >= 1; i--) {
+            System.out.println(i);
+            
+        }
+    }
+    
+}
