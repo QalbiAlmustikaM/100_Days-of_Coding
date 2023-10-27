709543183aeb901a5628382287196f2c1d8a54a8
QalbiAlmustikaM <qalbimustika25@gmail.com>
2023-10-27 14:43:53 +0800

    Day19

---
+++ Day19/LuasPersegiPanjang.java
@@ -0,0 +1,15 @@
+package Day19;
+
+public class LuasPersegiPanjang {
+    public static void main(String[] args) {
+        // Deklarasi Variabel
+        int panjang = 12;
+        int lebar = 6;
+        
+        // Hitung Luas Persegi Panjang
+        int luas = panjang * lebar;
+        
+        // Tampilkan Hasi
+        System.out.println("Luas Persegi Panjang: " + luas);
+    }
+}
