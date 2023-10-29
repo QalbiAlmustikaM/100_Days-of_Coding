090118d06ab026fc35559d86c44c723f767e9a1a
QalbiAlmustikaM <qalbimustika25@gmail.com>
2023-10-29 09:29:04 +0800

    Day21

---
+++ Day21/DecrementExample.java
@@ -0,0 +1,11 @@
+package Day21;
+
+public class DecrementExample {
+    public static void main(String[] args) {
+        for (int i = 15; i >= 0; i -= 3) {
+            System.out.println("Nilai i: " + i);
+            
+        }
+    }
+    
+}
