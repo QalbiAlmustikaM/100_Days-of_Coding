5eb59276177a4bb9d194538145ac789d3e45d9f8
QalbiAlmustikaM <qalbimustika25@gmail.com>
2023-10-31 17:31:33 +0800

    Day23

---
+++ Day23/DoWhileExample.java
@@ -0,0 +1,14 @@
+package Day23;
+
+public class DoWhileExample {
+    public static void main(String[] args) {
+        // Inisialisasi variabel i
+        int i = 1;
+        
+        do{
+            // Cetak nilai i
+            System.out.println(i);
+            i++;
+        } while (i <= 7);
+    }
+}
