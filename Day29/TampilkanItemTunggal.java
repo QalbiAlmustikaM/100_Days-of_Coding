b3ae3034b456f7e1d71b4e653ce980840ceba6b3
QalbiAlmustikaM <qalbimustika25@gmail.com>
2023-11-06 08:05:54 +0800

    Day29

---
+++ Day29/TampilkanItemTunggal.java
@@ -0,0 +1,12 @@
+package Day29;
+
+public class TampilkanItemTunggal {
+    public static void main(String[] args) {
+        
+        // Membuat array
+        int[] array = {1, 2, 3, 4, 5, 6};
+
+        // Menampilkan item tunggal
+        System.out.println(array[5]);
+    }
+}
