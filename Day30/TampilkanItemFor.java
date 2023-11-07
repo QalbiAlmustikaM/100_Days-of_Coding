feb6628d0bb0782b984c0329c85ccb8709e295b9
QalbiAlmustikaM <qalbimustika25@gmail.com>
2023-11-07 11:03:52 +0800

    Day30

---
+++ Day30/TampilkanItemFor.java
@@ -0,0 +1,15 @@
+package Day30;
+
+public class TampilkanItemFor {
+    static public void main(String[] args) {
+        // Buatlah array item
+        String[] items = {"Buku", "Pensil", "Penghapus", "Rak Buku", "Kursi"};
+
+        // Tampilkan item dengan perulangan for
+        for (int i = 0; i < items.length; i++) {
+            System.out.println(items[i]);
+        }
+        
+    }
+    
+}
