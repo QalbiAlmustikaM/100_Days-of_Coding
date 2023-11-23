788edcb7673a16e4101ee46a2322bb2816552bad
QalbiAlmustikaM <qalbimustika25@gmail.com>
2023-11-23 23:05:35 +0800

    Day46

---
+++ Day46/EqualsExample.java
@@ -0,0 +1,12 @@
+package Day46;
+
+public class EqualsExample {
+    public static void main(String[] args) {
+        String s1 = new String("javat");
+        String s2 = new String("python");
+
+        //Membandingkan S1 dan s2 menggunakan eguals
+        System.out.println(s1.equals(s2));
+    }
+    
+}
