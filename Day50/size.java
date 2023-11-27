ab1720b8676a7aee18edf9c7f92d3b792ebe8148
QalbiAlmustikaM <qalbimustika25@gmail.com>
2023-11-27 16:31:56 +0800

    Day50

---
+++ Day50/size.java
@@ -0,0 +1,20 @@
+package Day50;
+
+import java.util.ArrayList;
+
+public class size {
+    public static void main(String[] args) {
+        ArrayList<String> daftarNama = new ArrayList<>();
+
+        daftarNama.add("Qalbi");
+        daftarNama.add("Tika");
+        daftarNama.add("Lubi");
+
+        // Mengambil ukuran daftar
+        int ukuran = daftarNama.size();
+
+        // Mencetak ukuran tersebut
+        System.out.println(ukuran);
+    }
+    
+}
