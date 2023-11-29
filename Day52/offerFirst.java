a9cc99ab5c15c9f2d0575b6ae9a3bae18bbc94f2
QalbiAlmustikaM <qalbimustika25@gmail.com>
2023-11-29 09:29:31 +0800

    Day52

--- Day52/offerFirst.java
+++ Day52/offerFirst.java
@@ -1,18 +1,20 @@
-package Day51;
+package Day52;
 
 import java.util.LinkedList;
 
-public class ProgramOffer {
+public class offerFirst {
     public static void main(String[] args) {
         LinkedList<String> list = new LinkedList<>();
         list.add("A");
         list.add("B");
         list.add("C");
-
-        // Menambahkan elemen "D" ke akhir LinkedList m
-        list.offer("D");
         
+        // Menambahkan elemen "E" ke awal LinkedList 
+        list.offerFirst("E");
+
+        // Menambahkan elemen "F" ke akhir LinkedList 
+        list.offerLast("F");
+
         System.out.println(list);
     }
-    
 }
