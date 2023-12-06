a4c9be3ec6c71d607b5d9cf437ba7e26494d6c4b
QalbiAlmustikaM <qalbimustika25@gmail.com>
2023-12-06 22:55:58 +0800

    Day59

---
+++ Day59/PengambilanKeputusan.java
@@ -0,0 +1,17 @@
+package Day59;
+
+public class PengambilanKeputusan {
+    public static void main(String[] args) {
+        // Deklarasi variabel
+        int usia = 25;
+        double penghasilan = 5000000;
+
+        // Pengambilan keputusan
+        if (usia >= 21 && penghasilan >= 3000000) {
+            System.out.println("Pemohon memenuhi syarat untuk mengajukan pinjaman.");
+        } else {
+            System.out.println("Pemohon tidak memenuhi syarat untuk mengajukan pinjaman.");
+        }
+     
+    }
+}
