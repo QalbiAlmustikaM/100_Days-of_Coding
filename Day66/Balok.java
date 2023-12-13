ab8db0e2a9d8e9fe49c8a6a57773edaf811907f1
QalbiAlmustikaM <qalbimustika25@gmail.com>
2023-12-13 19:28:08 +0800

    Day66

---
+++ Day66/Balok.java
@@ -0,0 +1,16 @@
+package Day66;
+
+public class Balok {
+    public static void main(String[] args) {
+        int panjang = 20;
+        int lebar = 5;
+        int tinggi = 3;
+
+        // Hitung volume balok
+        int volume = panjang * lebar * tinggi;
+
+        // Tampilkan hasil
+        System.out.println("Volume balok: " + volume);
+    }
+    
+}
