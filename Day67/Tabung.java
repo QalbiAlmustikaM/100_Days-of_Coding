9011c40dc4376063153c3f3805a6f2e1e8f40423
QalbiAlmustikaM <qalbimustika25@gmail.com>
2023-12-14 21:41:14 +0800

    Day67

---
+++ Day67/Tabung.java
@@ -0,0 +1,18 @@
+package Day67;
+
+
+public class Tabung {
+    public static void main(String[] args) {
+        // Masukan jari-jari dan tinggi tabung
+        int jari_jari = 5;
+        int tinggi = 3;
+
+        // Hitung volume tabung
+        double volume = Math.PI * jari_jari * jari_jari * tinggi;
+
+        // Tampilkan hasil
+        System.out.println("Volume tabung: " + volume);
+    
+    }
+    
+}
