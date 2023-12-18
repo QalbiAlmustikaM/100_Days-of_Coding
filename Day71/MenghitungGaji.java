ecafb2dda605fe00a92dee9e2b36bcbaae484d0c
QalbiAlmustikaM <qalbimustika25@gmail.com>
2023-12-18 20:57:07 +0800

    Day71

---
+++ Day71/MenghitungGaji.java
@@ -0,0 +1,16 @@
+package Day71;
+
+import java.util.Scanner;
+
+public class MenghitungGaji {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan jumlah jam kerja per minggu: ");
+        int jamKerja = input.nextInt();
+        System.out.print("Masukkan upah per jam: ");
+        int upahPerJam = input.nextInt();
+        int gaji = jamKerja * upahPerJam;
+        System.out.println("Gaji yang diterima adalah: " + gaji);
+    }
+}
+  
