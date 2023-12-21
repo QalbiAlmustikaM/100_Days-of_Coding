27e6a7f1a9a808534afbe32bdc97c21d2d2d2121
QalbiAlmustikaM <qalbimustika25@gmail.com>
2023-12-21 23:14:02 +0800

    Day74

---
+++ Day74/Aritmatika.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Day74;
+
+/**
+ *
+ * @author Qalbi Tika025
+ */
+public class Aritmatika {
+    
+}
