cd0807d7c2ab7467f9b780f7e12b72133e3f390c
QalbiAlmustikaM <qalbimustika25@gmail.com>
2023-12-24 11:06:01 +0800

    Day77

---
+++ Day77/VolumeSilinder.java
@@ -0,0 +1,16 @@
+package Day77;
+
+public class VolumeSilinder {
+    public static void main(String[] args) {
+        // Deklarasi variabel
+        float r = 5;
+        float h = 10;
+
+        // Hitung volume silinder
+        float volume = (float) (Math.PI * r * r * h);
+
+        // Tampilkan volume silinder
+        System.out.println("Volume silinder = " + volume);
+    }
+    
+}
