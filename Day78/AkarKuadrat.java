30bafd6b5c2595452ef3077f7eab83754d7c212f
QalbiAlmustikaM <qalbimustika25@gmail.com>
2023-12-25 18:47:24 +0800

    Day78

---
+++ Day78/AkarKuadrat.java
@@ -0,0 +1,15 @@
+package Day78;
+
+public class AkarKuadrat {
+    public static void main(String[] args) {
+        // Deklarasi variabel
+        float x = 16;
+
+        // Hitung akar kuadrat
+        float akar = (float) Math.sqrt(x);
+
+        // Tampilkan akar kuadrat
+        System.out.println("Akar kuadrat dari " + x + " adalah " + akar);
+    }
+}
+
