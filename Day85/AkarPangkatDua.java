8ba912fd5462dc92a935122853d976c73641d3ad
QalbiAlmustikaM <qalbimustika25@gmail.com>
2024-01-01 20:06:28 +0800

    Day85

---
+++ Day85/AkarPangkatDua.java
@@ -0,0 +1,14 @@
+package Day85;
+
+public class AkarPangkatDua {
+    public static void main(String[] args) {
+        int bilangan = 16;
+
+        // Cari akar pangkat dua
+        int akar = (int) Math.sqrt(bilangan);
+
+        // Tampilkan akar pangkat dua
+        System.out.println("Akar pangkat dua dari " + bilangan + " adalah: " + akar);
+        
+    }
+}
